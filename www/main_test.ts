cfb4b3d822e39f4bf667da4cab14f051f0e49793
Marvin Hagemeister <hello@marvinh.dev>
2023-08-12 21:30:28 +0200

    chore: use in process server for tests

--- www/main_test.ts
+++ www/main_test.ts
@@ -1,27 +1,17 @@
 import { assertArrayIncludes, assertEquals } from "$std/testing/asserts.ts";
-import { delay } from "$std/async/delay.ts";
-import { startFreshServer, withPageName } from "../tests/test_utils.ts";
+import { withFresh, withPageName } from "../tests/test_utils.ts";
 import { dirname, join } from "$std/path/mod.ts";
 import VERSIONS from "../versions.json" assert { type: "json" };
 
 const dir = dirname(import.meta.url);
 
-Deno.test("CORS should not set on GET /fresh-badge.svg", {
-  sanitizeResources: false,
-}, async () => {
-  const { serverProcess, lines, address } = await startFreshServer({
-    args: ["run", "-A", join(dir, "./main.ts")],
+Deno.test("CORS should not set on GET /fresh-badge.svg", async () => {
+  await withFresh("./www/main.ts", async (address) => {
+    const res = await fetch(`${address}/fresh-badge.svg`);
+    await res.body?.cancel();
+
+    assertEquals(res.headers.get("cross-origin-resource-policy"), null);
   });
-
-  const res = await fetch(`${address}/fresh-badge.svg`);
-  await res.body?.cancel();
-
-  assertEquals(res.headers.get("cross-origin-resource-policy"), null);
-
-  await lines.cancel();
-  serverProcess.kill("SIGTERM");
-  // await for the server to close
-  await delay(100);
 });
 
 Deno.test("shows version selector", {
